b742307246cb6854f04fddb37d056a878dce20ca
wilfriedy <wilfriedakpe3@gmail.com>
2023-07-18 18:06:39 +0100

    Python course

---
+++ COURSE/Course.py
@@ -0,0 +1,65 @@
+fruitsNames = ['mango', 'zoo' , 'apple', 'mango', 'banana', 'lemon', 'orange']
+ints = list(range(1,7))
+squaresComp = [2 for i in ints]
+# print(ints)
+squares = []
+for i in ints:
+    squares.append(i**2)
+# print(squares)
+
+print(squaresComp)
+
+
+
+
+
+
+# max min sum
+# print(max(fruitsNames))
+# print(min(fruitsNames))
+# print(sum(fruitsNames))
+# print(sum(ints))
+# fruitsNames.reverse()
+# fruitsNames.sort(reverse=True)
+# newBasket = fruitsNames[:]
+# print(newBasket)
+
+# fruitsNames.sort(reverse=True)
+# print(sorted(fruitsNames))
+# print(fruitsNames)
+# newFruits = fruitsNames[:]
+# print(f"{newFruits} is the new ones")
+# print(fruitsNames.index('lemon'))
+# print(fruitsNames[:-3])
+# print(fruitsNames)
+# fruitsNames.append('Pineapple')
+# integers = []
+# fruitsNames.insert(2, 'rice')
+# print(fruitsNames)
+# for item in fruitsNames:
+#     if item == 'mango':
+#         fruitsNames.remove('mango')
+# fruitsNames.remove('mango')
+# del fruitsNames[2]
+# poped = fruitsNames.pop(3)
+# print(f"{poped} was poped")
+# print('---------After deleting------------')
+
+
+
+# print(integers)
+# print('After')
+# for i in range(0,11):
+#     integers.append(i)
+#
+# print(integers)
+# for item in fruitsNames:
+#     print(f"{item} is nice")
+#     print('But it now')
+
+# fruitsNames[1] = 'Pineapple'
+# print(fruitsNames)
+# print(fruitsNames.index('mango'))
+# print(len(fruitsNames))
+
+# print(fruitsNames[len(fruitsNames) - 1])
